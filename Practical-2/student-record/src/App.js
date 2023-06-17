f5781af92144a4486a28bb247161265f956a4ce3
Class1 <Class1@gmail.com>
2023-06-17 19:06:53 +0530

    Added changes to form

--- Practical-2/student-record/src/App.js
+++ Practical-2/student-record/src/App.js
@@ -3,25 +3,35 @@ import './App.css';
 
 function App() {
   return (
+    <>
+    <h1 class="title">Admission Form</h1>
     <div class="container">
-      <h1 class="title">Admission Form</h1>
       <form class="form">
         <label htmlFor="name">Name:</label>
         <input type="text" id="name" />
 
+        <label htmlFor="dob">Date of Birth:</label>
+        <input type="date" id="dob" />
+
+        
         <label htmlFor="email">Email:</label>
         <input type="email" id="email" />
 
-        <label htmlFor="phone">Phone:</label>
+        <label htmlFor="phone">Phone number:</label>
         <input type="tel" id="phone" />
 
         <label htmlFor="address">Address:</label>
         <textarea id="address" rows="4"></textarea>
 
-        
+        <label htmlFor="blood">Blood Group:</label>
+        <input type="text" id="bgrp" />
+
+        <label htmlFor="father">Father's Name:</label>
+        <input type="text" id="fname" />
         <button type="submit">Submit</button>
       </form>
     </div>
+    </>
   );
 }
 

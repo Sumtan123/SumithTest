d472e3e784f0a2eda331f4f4c1405cf83170e6d9
Class1 <Class1@gmail.com>
2023-06-17 18:51:31 +0530

    Added forms

--- Practical-2/student-record/src/App.js
+++ Practical-2/student-record/src/App.js
@@ -3,21 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <h1 class="title">Admission Form</h1>
+      <form class="form">
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" />
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" />
+
+        <label htmlFor="phone">Phone:</label>
+        <input type="tel" id="phone" />
+
+        <label htmlFor="address">Address:</label>
+        <textarea id="address" rows="4"></textarea>
+
+        
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
